fad7237126ed6b910a959651ba0cb256ffc9ca97
CyphrRiot <CypherRiot@protonmail.com>
2026-04-09 16:31:26 -0700

    installer: revert to pkg_info -e (works with exact versions)

--- source/installer/packages.go
+++ source/installer/packages.go
@@ -59,6 +59,6 @@ func InstallPackages(packages []string) (int, error) {
 
 // isPackageInstalled checks if a package is already installed
 func isPackageInstalled(pkg string) bool {
-	cmd := exec.Command("sh", "-c", "pkg_info | grep -q \"^$pkg-\"")
+	cmd := exec.Command("pkg_info", "-e", pkg)
 	return cmd.Run() == nil
 }
